e7b43b60f3498cc8592dde905331013381e5f01e
Abderrazzaq Khastaf <akhastaf@e1r1p3.1337.ma>
2022-09-30 21:57:56 +0100

    fixing image upload in promotion

--- src/promotion/pipes/sharp.pipe.ts
+++ src/promotion/pipes/sharp.pipe.ts
@@ -7,7 +7,7 @@ import { v4 as uuidv4} from "uuid";
 @Injectable()
 export class SharpPipe implements PipeTransform<Express.Multer.File, Promise<String>> {
     async transform(image: Express.Multer.File): Promise<String> {
-        if (!image.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        if (image.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
             const filename = 'client/public/' + uuidv4() + '.webp';
             await sharp(image.buffer)
                 .resize(800)
